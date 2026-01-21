253652f7e6e5e662636e9ee7a2cb1b4cfd0d69f6
AASHRTIH SAI <aashrith2415@gmail.com>
2026-01-21 22:44:30 +0000

    blank

--- notebooks/priority_engine.py
+++ notebooks/priority_engine.py
@@ -20,9 +20,6 @@ def cal_priority(urgency,sentiment,age_hours):
         URGENCY_SCORE.get(urgency,1)*3+SENTIMENT_SCORE.get(sentiment,1)*2 + min(age_hours/24,2)
         )
 
-
-
-
 df = pd.read_csv("../data/tickets_with_sentiment.csv", encoding="latin1")
 
 df["age_hours"] = [random.randint(1, 72) for _ in range(len(df))]
@@ -38,4 +35,4 @@ df["priority_score"] = df.apply(
 
 df_sorted = df.sort_values("priority_score", ascending=False)
 
-print(df_sorted[["clean_text", "urgency", "sentiment", "priority_score"]].head())
+print(df_sorted[["clean_text","urgency","sentiment","priority_score"]].head())
